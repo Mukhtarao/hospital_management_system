f25a197de29e41e85e1b9adb9244492990d31e2c
Mukhtarao <bi220053@student.uthm.edu.my>
2026-06-18 03:04:51 +0800

    update forget password

--- forgot_password.php
+++ forgot_password.php
@@ -47,11 +47,11 @@ if (isset($_POST['submit'])) {
                 $mail->SMTPAuth = true;
                 $mail->Username = $smtpEmail;
                 $mail->Password = $smtpPassword;
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Port = 587;
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                $mail->Port = 465;
 
                 // Change to 2 only for testing. Keep 0 for normal use.
-                $mail->SMTPDebug = 2;
+                $mail->SMTPDebug = 0;
                 $mail->Debugoutput = 'html';
 
                 $mail->setFrom($smtpEmail, 'HGH Medical Portal');
